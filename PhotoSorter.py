58f66a0984fd95242007852953921b718c78c22c
P0WER1ISA <P0WER1ISA@protonmail.com>
2017-08-05 22:54:25 +0800

    fix bug

--- PhotoSorter.py
+++ PhotoSorter.py
@@ -20,75 +20,79 @@ logging.basicConfig(handlers=[logging.FileHandler(config.LOGFILE, 'w')],
 class PhotoSorter(object):
     """图片处理类"""
 
-    _source_dir, _save_dir, _save_mode, _copy_file, _rename_file = None
+    _save_mode = None
+    _copy_file = None
+    _rename_file = None
 
-    def __init__(self, source_dir=None, save_dir=None, save_mode=None, copy_file=None, rename_file=None) -> None:
+    def __init__(self, source_dir=None, save_dir=None) -> None:
         """初始化"""
         self._source_dir = source_dir
         self._save_dir = save_dir
-        self._save_mode = save_mode
-        self._copy_file = copy_file
-        self._rename_file = rename_file
 
         super().__init__()
-
-    @property
-    def input_dir(self):
-        return self._source_dir
-
-    @property
-    def output_dir(self):
-        return self._save_dir
 
     @property
     def save_mode(self):
         return self._save_mode
 
+    @save_mode.setter
+    def save_mode(self, save_mode):
+        self._save_mode = save_mode
+
     @property
     def copy_mode(self):
         return self._copy_file
+
+    @copy_mode.setter
+    def copy_mode(self, copy_mode):
+        self._copy_file = copy_mode
 
     @property
     def rename_file(self):
         return self._rename_file
 
-    def dispose(self):
-        """得到所有需要处理的文件"""
-        if os.path.exists(self.input_dir):
-            for root, dirs, files in os.walk(self.input_dir):
-                for name in files:
-                    filename = os.path.join(root, name)
-                    if os.path.isfile(filename) and os.path.splitext(filename)[1] in config.PHOTOEXT:
-                        with open(filename, 'rb') as f:
-                            tags = exifread.process_file(f)
-                            try:
-                                self.__save_file(filename, tags['EXIF DateTimeOriginal'])
-                            except KeyError:
-                                self.__save_file(filename, None)
-                            except Exception as e:
-                                logging.error('Error message:{0}.'.format(e))
-                                raise e
-        else:
-            raise IOError('Path does not exist: {0}'.format(self.input_dir))
+    @rename_file.setter
+    def rename_file(self, rename_file):
+        self._rename_file = rename_file
 
-    def __save_file(self, filename, datetime_original) -> None:
-        """保存处理后的文件"""
-        save_filepath, save_filename = self.__get_output_dir(datetime_original)
+    def __walk_files(self):
+        """递归批量处理文件"""
+        if os.path.exists(self._source_dir):
+            for dirpath, dirnames, filenames in os.walk(self._source_dir):
+                for name in filenames:
+                    filename = os.path.join(dirpath, name)
+                    yield filename
+        else:
+            raise IOError('Path does not exist: {0}'.format(self._source_dir))
 
-        if self.rename_file:
-            save_filename = ''.join([save_filename, os.path.splitext(filename)[1]])
-        else:
-            save_filename = os.path.basename(filename)
-        print('source:{0}   --->    to:{1}'.format(filename, save_filename))
-        if not os.path.exists(save_filepath):
-            try:
-                os.makedirs(save_filepath)
-            except:
-                raise IOError('Path does not create：{0}'.format(save_filepath))
-        if self.copy_mode:
-            shutil.copyfile(filename, os.path.join(save_filepath, save_filename))
-        else:
-            shutil.move(filename, os.path.join(save_filepath, save_filename))
+    def files_to_dispose(self) -> None:
+        """处理文件"""
+        i = 0
+        for fname in self.__walk_files():
+            file_ext = os.path.splitext(fname)[1].lower()
+            if file_ext in config.PHOTOEXT:
+                with open(fname, 'rb') as f:
+                    tags = exifread.process_file(f)
+                    try:
+                        datetime_original = tags['EXIF DateTimeOriginal']
+                    except KeyError:
+                        datetime_original = None
+                    """保存处理后的文件"""
+                    save_filepath, save_filename_without_ext = self.__get_output_dir(datetime_original)
+                    save_filename = os.path.basename(fname)
+                    if self.rename_file:
+                        save_filename = save_filename_without_ext + os.path.splitext(fname)[1].lower()
+                    if not os.path.exists(save_filepath):
+                        try:
+                            os.makedirs(save_filepath)
+                        except:
+                            raise IOError('Path does not create：{0}'.format(save_filepath))
+                    if self.copy_mode:
+                        shutil.copyfile(fname, os.path.join(save_filepath, save_filename))
+                    else:
+                        shutil.move(fname, os.path.join(save_filepath, save_filename))
+                    i += 1
+                    print('{0} >>> source:{1} -> to:{2}'.format(i, fname, os.path.join(save_filepath, save_filename)))
 
     def __get_output_dir(self, datetime_original) -> tuple:
         year, month, day = datetime.min.year, datetime.min.month, datetime.min.day
@@ -100,13 +104,13 @@ class PhotoSorter(object):
         except Exception as e:
             logging.error('Error message:{0}.'.format(e))
         if self.save_mode == 'year':
-            return os.path.join(self.output_dir, str(year)), ''.join(
+            return os.path.join(self._save_dir, str(year)), ''.join(
                 [str(year), str(month), str(day), str(hour), str(minute), str(second)])
         elif self.save_mode == 'month':
-            return os.path.join(self.output_dir, str(year), str(month)), ''.join(
+            return os.path.join(self._save_dir, str(year), str(month)), ''.join(
                 [str(year), str(month), str(day), str(hour), str(minute), str(second)])
         elif self.save_mode == 'day':
-            return os.path.join(self.output_dir, str(year), str(month), str(day)), ''.join(
+            return os.path.join(self._save_dir, str(year), str(month), str(day)), ''.join(
                 [str(year), str(month), str(day), str(hour), str(minute), str(second)])
         else:
             raise ValueError(
@@ -122,6 +126,7 @@ if __name__ == "__main__":
     parser.add_argument('-R', '--rename', help='文件是否按照日期重命名', action='store_true')
     parser.add_argument('-V', '--version', help='显示程序版本号', action='version', version='%(prog)s v1.0')
     args = parser.parse_args()
+
     _source_dir = args.source_dir
     _save_dir = args.save_dir
     if args.mode in ('year', 'month', 'day'):
@@ -131,5 +136,9 @@ if __name__ == "__main__":
     _copy_file = bool(args.copy)
     _rename_file = bool(args.rename)
 
-    ps = PhotoSorter(_source_dir, _save_dir, _save_mode, _copy_file, _rename_file)
-    ps.dispose()
+    ps = PhotoSorter(_source_dir, _save_dir)
+    ps.save_mode = _save_mode
+    ps.copy_mode = _copy_file
+    ps.rename_file = _rename_file
+
+    ps.files_to_dispose()
